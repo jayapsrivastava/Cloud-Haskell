d5e4ba527d33e729721cdbca017f43540c8ac573
jayapsrivastava <jaya.srivastava@braamresearch.com>
2014-07-25 14:42:25 +0100

    process

---
+++ DNAChannelFile1.hs
@@ -0,0 +1,59 @@
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE DeriveDataTypeable         #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE ForeignFunctionInterface #-}
+
+module DNAChannelFile (itemSize, readData, roundUpDiv, chunkOffset, chunkSize) where
+
+import Data.Binary
+import Data.Typeable
+import Foreign.Ptr
+import Foreign.C.Types
+import Foreign.C.String
+import qualified Data.Vector.Storable as S
+import qualified Data.Vector.Storable.Mutable as MS
+import Control.Distributed.Process
+
+newtype Message = Ping ProcessId deriving (Eq, Ord, Show, Binary, Typeable)
+newtype FileMsg = FileMsg (S.Vector Double)  deriving (Show,Typeable)
+
+itemSize :: Int
+itemSize = 8
+-- divide up a file in chunkCount chunks
+-- functions to read chunkSize * itemSize from a file at offset chunkSize * itemSize
+roundUpDiv :: Int -> Int -> Int
+roundUpDiv a b = - div (-a) b
+
+chunkOffset :: Int -> Int -> Int -> Int
+chunkOffset chunkCount itemCount chunkNo
+    | chunkNo > chunkCount || chunkNo < 1 = -1
+    | otherwise = (chunkNo -1 ) * roundUpDiv itemCount chunkCount
+
+
+chunkSize :: Int -> Int -> Int -> Int
+chunkSize cC iC cN
+    |  cN < 1 || cN > cC = 0
+    |  cN > div iC (roundUpDiv iC cC) = iC - (cN -1) * (roundUpDiv iC cC)
+    |  otherwise = roundUpDiv iC cC
+
+-- read a buffer from a file into pinned memory
+-- arguments: buffer ptr, size, offset, path
+foreign import ccall unsafe "buffer-io.h read_data"
+    c_read_data :: Ptr CDouble -> CInt -> CInt -> CString -> IO ()
+
+readData :: Int -> Int -> String -> IO (S.Vector Double)
+readData n o p = do
+	mv <- MS.new n :: IO (MS.IOVector Double)
+  	MS.unsafeWith mv $ \ptr ->
+    -- Here I assume that CDouble and Double are same thing (it is)
+    --     -- and blindly cast pointer
+        	withCString p (c_read_data (castPtr ptr) (fromIntegral n) (fromIntegral o))
+        S.unsafeFreeze mv
+
+createFileReadChannel :: ProcessId -> Int -> Int -> String -> Process(S.Vector Double)
+createFileReadChannel pid chunkSize chunkOffset file = do
+	iov <- liftIO $ readData chunkSize chunkOffset file
+	
+	(Ping pid) <- expect
+	send pid (FileMsg (S.iov))
